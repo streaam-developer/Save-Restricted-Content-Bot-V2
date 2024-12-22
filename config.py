ab9285a2bf7ff89b739a1162d082202d3bec274b
streaam-developer <163761596+streaam-developer@users.noreply.github.com>
2024-12-22 17:40:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,11 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+API_ID = int(getenv("API_ID", "904789"))
+API_HASH = getenv("API_HASH", "2262ef67ced426b9eea57867b11666a1")
+BOT_TOKEN = getenv("BOT_TOKEN", "7366608947:AAEZgQ8wJsPCn0Ki812QFSHFsVgpwuIusNg")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6936727037").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://fehebaw351:nHbjrujWqgqLR58H@cluster0.lekn97z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "-1002407665705")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002407665705"))
 DEFAULT_SESSION = getenv("DEFAULT_SESSION", "")

d3d09a65f0ee916a4c95ec571ce25694bbc51f76
streaam-developer <163761596+streaam-developer@users.noreply.github.com>
2024-12-26 01:50:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,13 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+API_ID = int(getenv("API_ID", "9976721"))
+API_HASH = getenv("API_HASH", "3ef17a8cdb938335bd8ba292e6d816aa")
+BOT_TOKEN = getenv("BOT_TOKEN", "7366608947:AAEZgQ8wJsPCn0Ki812QFSHFsVgpwuIusNg")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "6936727037").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://rajbharneeraj9jfb:63372572@cluster0.fnig6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "-1002407665705")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002407665705"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
